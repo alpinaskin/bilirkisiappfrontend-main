d1a89215e0ea80c0125031e83544aab232ad142c
alpinaskin <44949739+alpinaskin@users.noreply.github.com>
2022-04-06 17:02:04 +0300

    Update UserService.tsx

--- src/services/UserService.tsx
+++ src/services/UserService.tsx
@@ -2,7 +2,7 @@ import axios from 'axios'
 import { User } from 'src/models/User'
 import authHeader from 'src/services/AuthHeader'
 
-const API_URL = 'http://localhost:8080/api/v1/'
+const API_URL = 'https://springbilirkisiapp.herokuapp.com/api/v1/'
 
 const getRaporlar = () => {
   return axios.get(API_URL + 'raporlar', { headers: authHeader() })
